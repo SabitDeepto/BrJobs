dc1a9c7fba75a663884899f31db94be211fec675
Sabit Mahmud Deepto <deepto69@gmail.com>
2019-02-22 13:35:36 +0600

    is_freelancer can be added with update profile

--- .history/Test/forms_20190222131038.py
+++ .history/Test/forms_20190222131038.py
@@ -1,12 +1,12 @@
 from django import forms
 from .models import Profile
-from .models import User
+from django.contrib.auth.models import User
 
 
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('is_freelancer', 'is_compnay', 'username', 'first_name', 'last_name', 'email')
+        fields = ('is_compnay', ', 'username', 'first_name', 'last_name', 'email')
 
 
 class ProfileForm(forms.ModelForm):
--- .history/Test/forms_20190222131750.py
+++ .history/Test/forms_20190222131750.py
@@ -6,7 +6,7 @@ from .models import User
 class UserForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('is_freelancer', 'is_compnay', 'username', 'first_name', 'last_name', 'email')
+        fields = ('is_freelancer', 'username', 'first_name', 'last_name', 'email')
 
 
 class ProfileForm(forms.ModelForm):
