8ce2c40a648417e1bb275c5dde2660313529cbf6
SabitDeepto <deepto69@gmail.com>
2019-03-07 16:45:33 +0600

    showed success message with css

--- configurations/settings.py
+++ configurations/settings.py
@@ -141,7 +141,15 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 
 
 
+from django.contrib.messages import constants as messages
 
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
 
 #...
 SITE_ID = 1
